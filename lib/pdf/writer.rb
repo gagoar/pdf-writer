e0ed4f19293ee6b9e3b1d73c946e6c4a25c8b0be
Néstor Coppi <nestorcoppi@gmail.com>
2014-03-20 16:59:46 -0300

    Now with Barcode 3-9 font

--- lib/pdf/writer.rb
+++ lib/pdf/writer.rb
@@ -573,7 +573,7 @@ class PDF::Writer
     margins_pt(in2pts(top), in2pts(left), in2pts(bottom), in2pts(right))
   end
 
-    # Define the margins in points. This will move the #y pointer 
+    # Define the margins in points. This will move the #y pointer
     #
     #                                   # T  L  B  R
     #   pdf.margins_pt(36)              # 36 36 36 36
@@ -801,7 +801,7 @@ class PDF::Writer
         break if fontfile
       end
     end
-    
+
     if font =~ /afm/o and fontfile
         # Find the array of font widths, and put that into an object.
       first_char  = -1
@@ -2131,7 +2131,7 @@ class PDF::Writer
     #
     # Each time page numbers are started, a new page number scheme will be
     # started. The scheme number will be returned.
-  def start_page_numbering(x, y, size, pos = nil, pattern = nil, starting = nil)   
+  def start_page_numbering(x, y, size, pos = nil, pattern = nil, starting = nil)
     pos     ||= :left
     pattern ||= "<PAGENUM> of <TOTALPAGENUM>"
     starting  ||= 1
@@ -2216,10 +2216,10 @@ class PDF::Writer
   end
   private :page_number_search
 
-  def add_page_numbers            
+  def add_page_numbers
       # This will go through the @page_numbering array and add the page
       # numbers are required.
-    if @page_numbering                           
+    if @page_numbering
       page_count  = @pageset.size
       pn_tmp      = @page_numbering.dup
 
@@ -2228,19 +2228,19 @@ class PDF::Writer
           # First, find the total pages for this schemes.
         page = page_number_search(:stop_total, scheme)
 
-        if page                             
+        if page
           total_pages = page
         else
-          page = page_number_search(:stop_total_next, scheme)        
-          if page  
+          page = page_number_search(:stop_total_next, scheme)
+          if page
             total_pages = page
-          else                     
+          else
             total_pages = page_count - 1
           end
         end
 
         status  = nil
-        delta   = pattern = pos = x = y = size = nil 
+        delta   = pattern = pos = x = y = size = nil
         pattern = pos = x = y = size = nil
 
         @pageset.each_with_index do |page, index|
@@ -2250,11 +2250,11 @@ class PDF::Writer
           if info
             if info[:start]
               status = true
-              if info[:starting] 
-                delta = info[:starting] - index 
-              else 
-                delta = index 
-              end  
+              if info[:starting]
+                delta = info[:starting] - index
+              else
+                delta = index
+              end
 
               pattern = info[:pattern]
               pos     = info[:pos]
@@ -2274,7 +2274,7 @@ class PDF::Writer
 
           if status
               # Add the page numbering to this page
-            num   = index + delta.to_i 
+            num   = index + delta.to_i
             total = total_pages + num - index
             patt  = pattern.gsub(/<PAGENUM>/, num.to_s).gsub(/<TOTALPAGENUM>/, total.to_s)
             reopen_object(page.contents.first)
@@ -2339,6 +2339,12 @@ class PDF::Writer
   def text(text, options = {})
       # Apply the filtering which will make underlining (and other items)
       # function.
+
+    # If the text is for barcode3-9 apply * before and after
+    if text != '' && options[:font_type] == 'ArchonCode39Barcode'
+      text = "*#{text}*"
+    end
+
     text = preprocess_text(text)
 
     options ||= {}
@@ -2724,11 +2730,11 @@ class PDF::Writer
     # Save the PDF as a file to disk.
   def save_as(name)
     File.open(name, "wb") { |f| f.write self.render }
-  end 
+  end
 
-  # memory improvement for transaction-simple  
+  # memory improvement for transaction-simple
   def _post_transaction_rewind
     @objects.each { |e| e.instance_variable_set(:@parent,self) }
   end
-          
+
 end
